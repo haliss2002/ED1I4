f5203f906c261c97b192a48b37393298290f1b7a
haliss2002 <40603636+haliss2002@users.noreply.github.com>
2021-10-20 19:32:54 -0300

    Tp 20/10

---
+++ projetoLista1/Contatos.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoLista1
+{
+    class Contatos
+    {
+        public List<Contato> agenda { get; }
+
+        public Contatos()
+        {
+            this.agenda = new List<Contato>();
+        }
+        public bool adicionar(Contato c)
+        {
+            if (Equals(c))
+            {
+                return false;
+            }
+            agenda.Add(c);
+            return true;
+        }
+        public Contato pesquisar(Contato c)
+        {
+            foreach (Contato contato in agenda)
+            {
+                if (c.email == contato.email)
+                {
+                    return contato;
+                }
+            }
+            return null;
+        }
+
+        public bool alterar(Contato c)
+        {
+            bool podeAlterar = false;
+            Console.Write("Digite o email da pessoa a ser alterada: ");
+            string resp = Console.ReadLine();
+            foreach (Contato ct in agenda)
+            {
+                if (c.email == resp)
+                {
+                    Console.Write("Digite o telefone a ser alterado: ");
+                    c.telefone = Console.ReadLine();
+                    podeAlterar = true;
+                }
+            }
+            return podeAlterar;
+        }
+        public bool remover(Contato c)
+        {
+            bool podeRemover = false;
+            Console.Write("Digite o email da pessoa a ser removida: ");
+            string emailDelete = Console.ReadLine();
+            for (int i = 0; i < agenda.Count; ++i)
+            {
+                if (c.email == emailDelete)
+                {
+                    agenda.Remove(c);
+                    podeRemover = true;
+                }
+            }
+            return podeRemover;
+        }
+        public override bool Equals(object obj)
+        {
+            return this.agenda.Equals(((Contato)obj).email);
+        }
+    }
+}
---
+++ projetoLista1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoLista1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            double escolha, fim = 0;
+            Data data = new Data();
+            Contato contato = new Contato();
+            Contatos contatos = new Contatos();
+            do
+            {
+                Console.WriteLine("0. Sair");
+                Console.WriteLine("1. Adicionar contato");
+                Console.WriteLine("2. Pesquisar contato");
+                Console.WriteLine("3. Alterar contato");
+                Console.WriteLine("4. Remover contato");
+                Console.WriteLine("5. Listar contatos");
+                x = Convert.ToInt32(Console.ReadLine());
+                if (x == 0)
+                {
+                    x = 10;
+                }
+                else if (x == 1)
+                {
+                    Console.Write("Digite email: ");
+                    string email = Console.ReadLine();
+                    Console.Write("Digite nome: ");
+                    string nome = Console.ReadLine();
+                    Console.Write("Digite telefone: ");
+                    string telefone = Console.ReadLine();
+                    Console.Write("Digite o dia: ");
+                    int dia = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Digite o mes: ");
+                    int mes = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Digite o ano: ");
+                    int ano = Convert.ToInt32(Console.ReadLine());
+                    data = new Data(dia, mes, ano);
+                    contato = new Contato(email, nome, telefone, data);
+                    Console.WriteLine(contatos.adicionar(contato));
+                }
+                else if (x == 2)
+                {
+                    Contato pesquisar = new Contato();
+                    Console.Write("Digite o email a ser pesquisado: ");
+                    string emailPesquisar = Console.ReadLine();
+                    pesquisar.email = emailPesquisar;
+                    Console.WriteLine(contatos.pesquisar(pesquisar).ToString());
+                }
+                else if (x == 3)
+                {
+                    Console.WriteLine(contatos.alterar(contato));
+                }
+                else if (x == 4)
+                {
+                    Console.WriteLine(contatos.remover(contato));
+                }
+                else if (x == 5)
+                {
+                    foreach (Contato ctt in contatos.agenda)
+                    {
+                        Console.WriteLine(ctt.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("O numero digitado é invalido.");
+                }
+            } while(x!=0);
+        }
+    }
+}
